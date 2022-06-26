466dc166188d769a5af4dc50df8b1427e4e0503f
duonglong753 <64320868+duonglong753@users.noreply.github.com>
2022-06-26 15:51:43 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./views/Nav";
 function App() {
+  const handleEventOnClick = (event) => {
+    console.log(">>> click me", event.target.value);
+  };
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello world!</h1>
+        <input
+          type="text"
+          value="Long"
+          onClick={(event) => handleEventOnClick(event)}
+        />
+        <button type="button" onClick={handleEventOnClick}>
+          Click me
+        </button>
       </header>
     </div>
   );
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,8 +1,8 @@
 import "./Nav.scss";
 const Nav = () => {
   return (
-    <div class="topnav">
-      <a class="active" href="#home">
+    <div className="topnav">
+      <a className="active" href="#home">
         Home
       </a>
       <a href="#news">News</a>
