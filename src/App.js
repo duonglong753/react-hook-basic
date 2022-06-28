4f43a32ab4fc7ccdaa3d4ab5d4eee87b44ee0d8d
duonglong753 <64320868+duonglong753@users.noreply.github.com>
2022-06-28 15:19:52 +0700

    render loop with react hook

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import { useState } from "react";
 function App() {
   const [name, setName] = useState("Long");
   const [input, setInput] = useState("");
+  const [todos, setToDos] = useState([
+    { id: "todos1", title: "watching tv" },
+    { id: "todos2", title: "doing homework" },
+    { id: "todos3", title: "playing game" },
+  ]);
   const handleEventOnClick = (event) => {
-    setName(input);
-    console.log(">>> click me", input);
+    if (!input) {
+      alert("empty input!");
+      return;
+    }
+    //hook doesn't merge state so we use spread syntax array
+    let newtodo = { id: "abc", title: input };
+    setToDos([...todos, newtodo]);
+    setInput("");
   };
   const handleOnChange = (event) => {
     setInput(event.target.value);
@@ -19,6 +30,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world! {name}</h1>
+        <div className="todos-container">
+          {todos.map((todo) => {
+            console.log(">>> check todo list:", todo);
+            return (
+              <li className="todos-child" key={todo.id}>
+                {todo.title}
+              </li>
+            );
+          })}
+        </div>
         <input
           type="text"
           value={input}
