2ba77e6ca08b5d84e7b2e9f88f1cf78d91aeef26
duonglong753 <duonglong753@gmail.com>
2022-07-10 19:01:06 +0700

    loading message

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -3,56 +3,70 @@ import axios from "axios";
 import moment from "moment";
 function Covid() {
   const [dataCovid, setDataCovid] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getAPI = async () => {
-      try {
-        let res = await axios.get(
-          "https://api.covid19api.com/country/vietnam?from=2021-10-01T00:00:00Z&to=2021-10-30T00:00:00Z"
-        );
-        // console.log(">>>check res: ", res.data);
-        let data = res && res.data ? res.data : [];
-        if (data && data.length > 0) {
-          data.map((item) => {
-            item.Date = moment(item.Date).format("DD/MM/YYYY");
-            return item;
-          });
+      setTimeout(async () => {
+        try {
+          let res = await axios.get(
+            "https://api.covid19api.com/country/vietnam?from=2021-10-01T00:00:00Z&to=2021-10-30T00:00:00Z"
+          );
+          console.log(">>>check res: ", res.data);
+          let data = res && res.data ? res.data : [];
+          if (data && data.length > 0) {
+            data.map((item) => {
+              item.Date = moment(item.Date).format("DD/MM/YYYY");
+              return item;
+            });
+          }
+          setDataCovid(data);
+          setLoading(false);
+        } catch (error) {
+          console.error(error);
         }
-        setDataCovid(data);
-        // console.log(">>>check data:", data);
-      } catch (error) {
-        console.error(error);
-      }
+      }, 3000);
     };
     getAPI();
   }, []);
   return (
-    <table>
-      {console.log(">>>check data covid: ", dataCovid)}
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Active</th>
-          <th>Confirmed</th>
-          <th>Deaths</th>
-          <th>Recovered</th>
-        </tr>
-      </thead>
-      <tbody>
-        {dataCovid &&
-          dataCovid.length > 0 &&
-          dataCovid.map((item) => {
-            return (
-              <tr key={item.ID}>
-                <td>{item.Date}</td>
-                <td>{item.Active}</td>
-                <td>{item.Confirmed}</td>
-                <td>{item.Deaths}</td>
-                <td>{item.Recovered}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+    <>
+      <h3>Covid-19 tracking in Viet Nam</h3>
+      <table>
+        {console.log(">>>check data covid: ", dataCovid)}
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Active</th>
+            <th>Confirmed</th>
+            <th>Deaths</th>
+            <th>Recovered</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading === false &&
+            dataCovid &&
+            dataCovid.length > 0 &&
+            dataCovid.map((item) => {
+              return (
+                <tr key={item.ID}>
+                  <td>{item.Date}</td>
+                  <td>{item.Active}</td>
+                  <td>{item.Confirmed}</td>
+                  <td>{item.Deaths}</td>
+                  <td>{item.Recovered}</td>
+                </tr>
+              );
+            })}
+          {loading === true && (
+            <tr>
+              <td colSpan={5} style={{ textAlign: "center" }}>
+                Loading...
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
   );
 }
 
